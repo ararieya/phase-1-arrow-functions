de1858762441086a403ccc29735cf2408a549dd4
Ashley Rarieya <ararieya@gmail.com>
2023-09-19 19:59:15 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide(num1, num2) {
+    return num1 / num2;
+  }
+
+console.log(divide(2000,100))
+
+const nums=[2]
+const squares = nums.map(x => x * x);
+
+console.log(squares);
+ 
+const add=(parameter1,parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+}  
+console.log(add(3,4));
+
